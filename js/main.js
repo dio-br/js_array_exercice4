332f2d25ceb2fc86b255cd9d63b9932f8be8b824
Mohammed <el-med7@outlook.com>
2020-12-30 15:44:51 +0100

    exo

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let tablo = [`oui`, `non`, `toi`]
+console.log(tablo);
+
+// ##
+
+tablo.push(`dio`)
+console.log(tablo);
+
+// ##
+
+tablo.splice(1, 1, `lui`)
+console.log(tablo);
+
+// ##
+
+tablo.splice(2, 1)
+console.log(tablo);
+
+// ##
+
